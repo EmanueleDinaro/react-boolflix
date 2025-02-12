c5b8eee1556d009da317d8576eb1e50fe7162f2d
Emanuele Dinaro <ema.dinaro@gmail.com>
2025-02-12 18:23:32 +0100

    fix some issues

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -14,17 +14,26 @@ export default function Main() {
               alt={film.title}
             />
             <h3 className="film-title">{film.title}</h3>
-            <h4 className="film-original-title">{film.original_title}</h4>
-            <img
-              className="film-language"
-              src={`https://purecatamphetamine.github.io/country-flag-icons/3x2/${film.original_language.toUpperCase()}.svg`}
-              alt={film.original_language}
-            />
-            <h4 className="film-vote">{film.vote_average}</h4>
+            <h4 className="film-original-title">
+              Titolo originale: {film.original_title}
+            </h4>
+            <div className="language">
+              <h4>Lingua: </h4>
+
+              <img
+                className="film-language"
+                src={`https://purecatamphetamine.github.io/country-flag-icons/3x2/${
+                  film.original_language === "en"
+                    ? "GB"
+                    : film.original_language.toUpperCase()
+                }.svg`}
+                alt={film.original_language}
+              />
+            </div>
+            <h4 className="film-vote">Voto: {film.vote_average}</h4>
           </li>
         ))}
       </ul>
-      ;
     </main>
   );
 }
