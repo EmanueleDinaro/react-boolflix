e5159389258e261173cf61e3f0ab62d67ef4ac62
Emanuele Dinaro <ema.dinaro@gmail.com>
2025-02-12 12:32:55 +0100

    feat: add axios call and searchbar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,37 @@
+import { useState } from "react";
+
 export default function SearchBar() {
+  const [search, setSearch] = useState("");
+  const [films, setFilms] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .get("https://api.themoviedb.org/3/search/movie", {
+        params: {
+          api_key: "e99307154c6dfb0b4750f6603256716d",
+          language: "it-IT",
+          query: search,
+        },
+      })
+      .then((res) => setFilms(res.data.results));
+  };
+
   return (
-    <form>
-      <input type="text" />
+    <form onSubmit={handleSubmit}>
+      <input
+        name="search"
+        type="search"
+        placeholder="Cerca..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
       <button type="submit">Invio</button>
+      <ul>
+        {films.map((film) => {
+          <li key={film.id}>{film.title}</li>;
+        })}
+      </ul>
     </form>
   );
 }
