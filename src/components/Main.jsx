9b224d811f4f4a1fbb19ea1ffadb60d607aea8c5
Emanuele Dinaro <ema.dinaro@gmail.com>
2025-02-12 18:03:23 +0100

    feat: add card for search film

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -7,9 +7,20 @@ export default function Main() {
     <main>
       <ul>
         {films.map((film) => (
-          <li key={film.id}>{film.title}</li>
+          <li key={film.id} className="film-card">
+            <img
+              className="film-image"
+              src={`https://image.tmdb.org/t/p/w342/${film.poster_path}`}
+              alt={film.title}
+            />
+            <h3 className="film-title">{film.title}</h3>
+            <h4 className="film-original-title">{film.original_title}</h4>
+            <h4 className="film-language">{film.original_language}</h4>
+            <h4 className="film-vote">{film.vote_average}</h4>
+          </li>
         ))}
       </ul>
+      ;
     </main>
   );
 }
